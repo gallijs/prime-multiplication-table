2ca4d8796f133d1effe0150a48ff143859857a81
Juan Gallisa <j.s.gallisa@gmail.com>
2023-02-15 11:57:45 -0400

    Add a parse method to PrimeTable
    This method uses ruby OptionParser to create a help message for the use
    of the script and to receive whole integer N.
    
    TODO: N will be used to generate a list of prime number of N length

--- lib/prime_table.rb
+++ lib/prime_table.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
+require "optparse"
 require_relative "prime_table/version"
 
 module PrimeTable
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Define the options and parse the command line arguments
+  def self.parse(options)
+    n = 3
+
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: prime_table.rb [options]"
+
+      opts.on("-n N", Integer, "Number of primes to generate (default: 3)") do |value|
+        n = value
+      end
+
+      opts.on("-h", "--help", "Prints this help") do
+        puts opts
+        exit
+      end
+    end
+
+    opt_parser.parse!(options)
+    return n
+  end
 end
+
+options = PrimeTable.parse(ARGV)
+puts options
--- spec/prime_table_spec.rb
+++ spec/prime_table_spec.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "optparse"
 require_relative "../lib/prime_table"
 
 describe "PrimeTable" do
-  xit "return a multiplication table of prime numbers" do
+  describe ".parse" do
+    context "when the n parameter is a positive number" do
+      it "returns the integer" do
+        expect(PrimeTable.parse %w[-n 10]).to eq 10
+      end
+    end
   end
+  xit "return a multiplication table of prime numbers"
 end
